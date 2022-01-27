ee4919a6daf7412c548f753cdfa888cac9e71e33
Amber Naranjo <adnaranjo@amarillocollege.com>
2022-01-27 10:30:56 -0600

    Update pytech_insert.py

--- module_5/pytech_insert.py
+++ module_5/pytech_insert.py
@@ -7,6 +7,7 @@ db = client.pytech
 
 guy = {
 "first_name" : "Guy",
+"last_name" : "Smith",
 "student_id" : 1007
 }
 guy_student_id = db.students.insert_one(guy).inserted_id
@@ -14,6 +15,7 @@ print(guy_student_id)
 
 gal = {
 "first_name" : "Gal",
+"last_name" : "Smith",
 "student_id" : 1008
 }
 gal_student_id = db.students.insert_one(gal).inserted_id
@@ -21,6 +23,7 @@ print(gal_student_id)
 
 gus = {
 "first_name" : "Gus",
+"last_name" : "Smith",
 "student_id" : 1009
 }
 gus_student_id = db.students.insert_one(gus).inserted_id
