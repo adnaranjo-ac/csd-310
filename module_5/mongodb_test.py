54e72f6ac721cbb76eea240d787be80724502b7a
Amber Naranjo <78166584+adnaranjo-ac@users.noreply.github.com>
2022-01-27 09:44:16 -0600

    Delete module_5 directory

--- module_5/mongodb_test.py
+++
@@ -1,8 +0,0 @@
-import pymongo
-from pymongo import MongoClient
-
-url = "mongodb+srv://admin:admin@cluster0.xjcyc.mongodb.net/pytech"
-client = MongoClient(url)
-db = client.pytech
-
-print(db.list_collection_names)
