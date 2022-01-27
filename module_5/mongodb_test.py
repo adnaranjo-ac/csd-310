f739b01d39b5e3c9700845fa748e163e359da4cf
Amber Naranjo <adnaranjo@amarillocollege.com>
2022-01-27 09:31:12 -0600

    Update mongodb_test.py

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,7 +1,7 @@
 import pymongo
 from pymongo import MongoClient
 
-url = "mongodb+srv://admin:<password>@cluster0.xjcyc.mongodb.net/test"
+url = "mongodb+srv://admin:admin@cluster0.xjcyc.mongodb.net/pytech"
 client = MongoClient(url)
 db = client.pytech
 
